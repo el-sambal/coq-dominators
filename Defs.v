2b729e16597f66ea1cce49b051edbc86d94aaf0d
el-sambal <AronHardeman@proton.me>
2025-01-24 10:14:36 +0100

    change definition of dominance and replace functions {i,s}dom by quantifiers

--- Defs.v
+++ Defs.v
@@ -6,7 +6,6 @@ Require Import PeanoNat.
 
 Section Dominators.
 
-(* TODO: make this a record in some way; have not yet figured that out *)
 Inductive flow_graph :=
   | flowgr
     (* The number of vertices in the flow graph, including the root.
@@ -26,56 +25,6 @@ Variable (finish_time: nat -> nat).
 Definition FG : flow_graph :=
   flowgr num_nodes tree_edges other_edges start_time finish_time.
 
-Definition flow_graph_is_valid (fg : flow_graph) : Prop := 
-  match fg with
-  | flowgr num_nodes tree_edges other_edges start_time finish_time
-  =>
-  (*
-     (* Each node is reachable from the root *)
-     forall n : nat, 0 <= n < num_nodes -> 
-     /\
-   *)
-     (* Each node except the root has exactly one parent. *)
-     (forall n : nat, (1 <= n < num_nodes) ->
-       (exists p : nat, forall p' : nat,
-         tree_edges p' n <-> p = p'))
-     /\
-     (* The root does not have a parent *)
-     (forall p : nat, ~(tree_edges p 0))
-     /\
-     (* All nodes outside the range [0,num_nodes)
-      * are not in the {tree,other}_edges relation *)
-     (forall n m : nat, (num_nodes <= n \/ num_nodes <= m) ->
-       ~(tree_edges n m) /\ ~(other_edges n m))
-     /\
-     (* There cannot be both a tree-edge and a non-tree-edge
-      * between the same pair of nodes *)
-     (forall n m : nat, ~(tree_edges n m /\ other_edges n m))
-     /\
-     (* For each node n with finish time f,
-      * all nodes reachable from n have a finish time < f *)
-     (forall p c : nat, (tree_edges p c \/ other_edges p c) -> 
-       finish_time c < finish_time p)
-     /\
-     (* For each node, its start time is less than its finish time *)
-     (forall n : nat, start_time n < finish_time n)
-     /\
-     (* Each start time and finish time is in the range [0, 2*num_nodes) *)
-     (forall n : nat, 0 <= start_time n < 2*num_nodes /\
-       0 <= finish_time n < 2*num_nodes)
-     /\
-     (* No duplicate start or finish times *)
-     (forall n m : nat, n <> m -> start_time n <> start_time m /\
-       start_time n <> finish_time m /\ finish_time n <> finish_time m)
-     /\
-     (* The root node has start time 0 *)
-     (start_time 0) = 0
-     /\
-     (* The parent of each non-root node has a lower start time *)
-     (forall n : nat, (1 <= n < num_nodes) ->
-       forall p : nat, tree_edges p n -> start_time p < start_time n)
-  end.
-Axiom FG_valid: flow_graph_is_valid FG.
 
 (* This node exists within the flow graph *)
 Definition node_in_fg (n : nat) := n < num_nodes.
@@ -94,6 +43,7 @@ Inductive reachable_in_tree : nat -> nat -> Prop :=
   | rit_trans (n m k : nat) :
       reachable_in_tree n m -> reachable_in_tree m k ->
         reachable_in_tree n k.
+
 
 (* there is an edge (either tree or non-tree) from A to B *)
 Notation "A ==> B" := (A --> B \/ A ~~> B) (at level 70).
@@ -115,6 +65,80 @@ Notation "A >:= B" := (start_time A >= start_time B) (at level 70).
 
 (* the start time of A is > the start time of B *)
 Notation "A >: B" := (start_time A > start_time B) (at level 70).
+
+Inductive path (a b:nat) : Type :=
+  | path_refl :
+      a = b -> path a b
+  | path_append (a' : nat) :
+      path a a' -> a' ==> b -> path a b.
+
+Fixpoint path_contains (a b k : nat) (p : path a b) : Prop :=
+  match p with 
+  | path_refl _ _ _ => b = k
+  | path_append _ _ a' p' _ =>
+      b = k \/ path_contains a a' k p'
+  end.
+
+Fixpoint path_is_in_tree (a b : nat) (p : path a b) : Prop :=
+  match p with 
+  | path_refl _ _ _ => True
+  | path_append _ _ a' p' _ =>
+      tree_edges a' b /\ path_is_in_tree a a' p'
+  end.
+
+Definition flow_graph_is_valid (fg : flow_graph) : Prop := 
+  match fg with
+  | flowgr num_nodes tree_edges other_edges start_time finish_time
+  =>
+     (* Each node is reachable from the root *)
+     (forall n : nat, node_in_fg n ->
+        exists p : path 0 n, path_is_in_tree 0 n p)
+     (* NOTE: the rest is commented out because I haven't yet used them
+      * in proofs. This way, we don't assume unnecessary stuff. *)
+     (*
+        /\
+        (* Each node except the root has exactly one parent. *)
+        (forall n : nat, (1 <= n < num_nodes) ->
+          (exists p : nat, forall p' : nat,
+            tree_edges p' n <-> p = p'))
+        /\
+        (* The root does not have a parent *)
+        (forall p : nat, ~(tree_edges p 0))
+        /\
+        (* All nodes outside the range [0,num_nodes)
+         * are not in the {tree,other}_edges relation *)
+        (forall n m : nat, (num_nodes <= n \/ num_nodes <= m) ->
+          ~(tree_edges n m) /\ ~(other_edges n m))
+        /\
+        (* There cannot be both a tree-edge and a non-tree-edge
+         * between the same pair of nodes *)
+        (forall n m : nat, ~(tree_edges n m /\ other_edges n m))
+        /\
+        (* For each node n with finish time f,
+         * all nodes reachable from n have a finish time < f *)
+        (forall p c : nat, (tree_edges p c \/ other_edges p c) -> 
+          finish_time c < finish_time p)
+        /\
+        (* For each node, its start time is less than its finish time *)
+        (forall n : nat, start_time n < finish_time n)
+        /\
+        (* Each start time and finish time is in the range [0, 2*num_nodes) *)
+        (forall n : nat, 0 <= start_time n < 2*num_nodes /\
+          0 <= finish_time n < 2*num_nodes)
+        /\
+        (* No duplicate start or finish times *)
+        (forall n m : nat, n <> m -> start_time n <> start_time m /\
+          start_time n <> finish_time m /\ finish_time n <> finish_time m)
+        /\
+        (* The root node has start time 0 *)
+        (start_time 0) = 0
+        /\
+        (* The parent of each non-root node has a lower start time *)
+        (forall n : nat, (1 <= n < num_nodes) ->
+          forall p : nat, tree_edges p n -> start_time p < start_time n)
+     *)
+  end.
+Axiom FG_valid: flow_graph_is_valid FG.
 
 (* sdom_candidate A B is a necessary condition for A = sdom(B) *)
 Inductive sdom_candidate : nat -> nat -> Prop :=
@@ -150,9 +174,14 @@ Inductive reachable_wo : nat -> nat -> nat -> Prop :=
 
 (* dom A B <=> A dominates B, i.e.,
  * every path from the root to B must go through A, and A<>B. *)
+Inductive dom : nat -> nat -> Prop :=
+  | is_dom (n m : nat) :
+      (forall p : path 0 m, path_contains 0 m n p) -> dom n m.
+(* (* old definition: not convenient *)
 Inductive dom : nat -> nat -> Prop := 
   | is_dom (n m : nat) : node_in_fg n -> node_in_fg m ->
       ~(reachable_wo n 0 m) -> n <> m -> dom n m.
+*)
 
 (* is_idom_of A B <=> A is the immediate dominator of B, i.e.,
  * A dominates B and every other dominator of B dominates A. *)
@@ -161,11 +190,13 @@ Inductive is_idom_of : nat -> nat -> Prop :=
       node_in_fg k -> dom k m -> dom k n) ->
         is_idom_of n m.
 
+(*
 (* Each node in the flowgraph has exactly one semidominator *)
 Theorem sdom_unique :
   forall n : nat, node_in_fg n ->
     (exists sd : nat, forall sd' : nat, sd = sd' <-> is_sdom_of sd' n).
 Proof. Admitted.
+*)
 
 (* Each node w <> r in the flowgraph has exactly one immediate dominator *)
 Theorem LT_Theorem1_Part1 :
@@ -173,13 +204,15 @@ Theorem LT_Theorem1_Part1 :
     (exists id : nat, forall id' : nat, id = id' <-> is_idom_of id' n).
 Proof. Admitted.
 
-(* The immediate dominator function *)
-Definition idom : nat -> nat. Admitted.
-Axiom idom_function : forall n : nat, is_idom_of (idom n) n.
-
-(* The semidominator function *)
-Definition sdom : nat -> nat. Admitted.
-Axiom sdom_function : forall n : nat, is_sdom_of (idom n) n.
+(* (* unused: we prefer to use quantifiers. *)
+  (* The immediate dominator function *)
+  Definition idom : nat -> nat. Admitted.
+  Axiom idom_function : forall n : nat, is_idom_of (idom n) n.
+  
+  (* The semidominator function *)
+  Definition sdom : nat -> nat. Admitted.
+  Axiom sdom_function : forall n : nat, is_sdom_of (idom n) n.
+*)
 
 (* Lengauer, Tarjan:
  * If v, w are vertices of G such that v <:= w, then any
@@ -188,19 +221,14 @@ Axiom sdom_function : forall n : nat, is_sdom_of (idom n) n.
 Theorem LT_Lemma1 : True (* TODO *).
 Proof. Admitted.
 
+(*
 (* Helper lemma for [LT_Lemma2] *)
 Lemma dominator_reachable : forall n m : nat,
   node_in_fg n -> node_in_fg m ->
     ~ reachable_wo n 0 m -> reachable_in_tree n m.
 Proof. 
-  intros.
-  (* Proof idea: suppose that [~ reachable_in_tree n m].
-     Of course, [reachable_in_tree 0 m]; this is a basic
-     property of trees that we have assumed. *)
-  assert (reachable_in_tree 0 m).
-  - destruct FG_valid.
-
 Admitted.
+*)
 
 (*
 (* Each immediate dominator is inside the flowgraph. *)
@@ -211,41 +239,60 @@ Proof. Admitted.
 (* Lengauer, Tarjan:
  * For any vertex w <> r, idom(w) -+> w.
  *)
-Theorem LT_Lemma2 : forall w : nat, node_in_fg w -> w <> 0 -> idom w -+> w.
+Theorem LT_Lemma2 :
+  forall w idomw : nat, is_idom_of idomw w -> node_in_fg w ->
+    w <> 0 -> idomw -+> w.
 Proof.
   intros.
-  (* First we prove that [dom (idom w) w] using [idom_function].
-   * It follows immediately from the def. of [dom] that [idom w <> w],
-   * and that [~(reachable_wo (idom w) 0 w)].
-   *)
-  assert (is_idom_of (idom w) w) by apply idom_function. 
-  destruct H1. destruct H1 as [H1 _]. destruct H1. (* :) *)
-  split.
-  - apply dominator_reachable.
-    apply H1.
-    apply H2.
-    apply H3.
-  - apply H4.
-Qed.
+  (*
+    Proof idea: all paths from 0 to w must go through idomw.
+    There exists at least one such path that only uses tree edges,
+    as each node is reachable from the root using only tree edges.
+    Therefore, this path must contain a subpath from idomw to w.
+  *)
+  assert (exists p : path 0 w, path_is_in_tree 0 w p) as exists_path_0_w.
+  { apply FG_valid.
+    apply H0. }
+  destruct exists_path_0_w as [path_0_w tree_path].
+  assert (path_contains 0 w idomw path_0_w) as idomw_in_path_0_w.
+  { destruct H as [idomw w].
+    destruct H.
+    destruct H as [idomw w].
+    apply (H path_0_w). }
+  
+
+
+
+
+
+
+
+
+Admitted.
 
 (* Lengauer, Tarjan:
  * For any vertex w <> r, sdom(w) -+> w.
  *)
-Theorem LT_Lemma3 : forall w : nat, (node_in_fg w /\ w <> 0) -> sdom w -+> w.
+Theorem LT_Lemma3 :
+  forall w sdomw : nat, is_sdom_of sdomw w ->
+   (node_in_fg w /\ w <> 0) -> sdomw -+> w.
 Proof. Admitted.
 
 (* Lengauer, Tarjan:
  * For any vertex w <> r, idom(w) -*> sdom(w).
  *)
-Theorem LT_Lemma4 : forall w : nat, (node_in_fg w /\ w <> 0) -> idom w -*> sdom w.
+Theorem LT_Lemma4 :
+  forall w idomw sdomw : nat, is_idom_of idomw w -> is_sdom_of sdomw w
+    -> (node_in_fg w /\ w <> 0) -> idomw -*> sdomw.
 Proof. Admitted.
 
 (* Lengauer, Tarjan:
  * Let vertices v,w satisfy v -*> w. Then v -*> idom(w) or idom(w) -*> idom(v).
  *)
 Theorem LT_Lemma5 :
-  forall v w : nat, (node_in_fg v /\ node_in_fg w /\ v -*> w) ->
-    (v -*> idom w \/ idom w -*> idom v).
+  forall v w idomw idomv : nat, is_idom_of idomw w ->
+    is_idom_of idomv v -> (node_in_fg v /\ node_in_fg w /\ v -*> w) ->
+      (v -*> idomw \/ idomw -*> idomv).
 Proof. Admitted.
 
 
@@ -265,10 +312,13 @@ Proof. Admitted.
  * satisfies sdom(u) >:= sdom(w). Then idom(w) = sdom(w).
  *)
 Theorem LT_Theorem2 :
-  forall w : nat, node_in_fg w ->
-    (
-      (w <> 0 /\ (forall u, (node_in_fg u /\ sdom w -+> u /\ u -*> w) -> sdom u >:= sdom w)
-    ) -> idom w = sdom w).
+  forall w idomw sdomw : nat, is_idom_of idomw w ->
+    is_sdom_of sdomw w -> node_in_fg w -> w <> 0 ->
+      (
+        forall u sdomu : nat, is_sdom_of sdomu u ->
+          (node_in_fg u /\ sdomw -+> u /\ u -*> w) -> sdomu >:= sdomw
+      )
+        -> idomw = sdomw.
 Proof. Admitted.
 
 (* Lengauer, Tarjan:
@@ -277,10 +327,12 @@ Proof. Admitted.
  * Then sdom(u) <:= sdom(w) and idom(u) = idom(w).
  *)
 Theorem LT_Theorem3 :
-  forall w u : nat, w <> 0 -> node_in_fg u -> node_in_fg w ->
-      (sdom w -+> u /\ u -*> w) ->
-        (forall u' : nat, (sdom w -+> u' /\ u' -*> w) -> u <:= u') ->
-    (sdom u <:= sdom w /\ idom u = idom w).
+  forall w u idomu idomw sdomu sdomw : nat, is_sdom_of sdomu u ->
+    is_sdom_of sdomw w -> is_idom_of idomu u -> is_idom_of idomw w ->
+      w <> 0 -> node_in_fg u -> node_in_fg w ->
+        (sdomw -+> u /\ u -*> w) ->
+          (forall u' : nat, (sdomw -+> u' /\ u' -*> w) -> u <:= u') ->
+    (sdomu <:= sdomw /\ idomu = idomw).
 Proof. Admitted.
 
 
@@ -291,34 +343,35 @@ Proof. Admitted.
  * idom(w) = idom(u) otherwise.
  *)
 Theorem LT_Corollary1 :
-  forall w u : nat, w <> 0 -> node_in_fg u -> node_in_fg w ->
-      (sdom w -+> u /\ u -*> w) ->
-        (forall u' : nat, (sdom w -+> u' /\ u' -*> w) -> u <:= u') ->
-    ((sdom w = sdom u -> idom w = sdom w) /\
-     (sdom w <> sdom u -> idom w = idom u)).
+  forall w u idomu idomw sdomu sdomw: nat, is_idom_of idomu u ->
+    is_idom_of idomw w -> is_sdom_of sdomu u -> is_sdom_of sdomw w ->
+      w <> 0 -> node_in_fg u -> node_in_fg w ->
+        (sdomw -+> u /\ u -*> w) ->
+          (forall u' : nat, (sdomw -+> u' /\ u' -*> w) -> u <:= u') ->
+    ((sdomw = sdomu -> idomw = sdomw) /\
+     (sdomw <> sdomu -> idomw = idomu)).
 Proof. Admitted.
 
-(* not including everything THE FINAL RESULT WE WANT TO PROVE *)
+Inductive is_minimum_of (n : nat) (P : nat -> Prop) : Prop :=
+  | is_min : P n -> (forall n' : nat, P n' -> n <= n') -> is_minimum_of n P.
+
 (* Lengauer, Tarjan:
  * If w is not the root node, then
-   w = min(
+   sdom w = min(
            {v | v ==> w and v <: w}
              union
            {sdom(u) | u >: w and there is an edge v ==> w such that u -*> v}
           ).
  *)
 Theorem LT_Theorem4 :
-  forall w : nat, w <> 0 -> node_in_fg w ->
+  forall w sdomw : nat, is_sdom_of sdomw w -> w <> 0 -> node_in_fg w ->
+  is_minimum_of (sdomw)
   (
-    (sdom w ==> w /\ sdom w <: w) \/
-      (exists u v : nat, node_in_fg u /\ node_in_fg v /\ u >: w /\ v ==> w /\ u -*> v)
-  )
-  /\
-  (
-  forall w' : nat, node_in_fg w' -> (
-      (sdom w' ==> w' /\ sdom w' <: w') \/
-        (exists u v : nat, node_in_fg u /\ node_in_fg v /\ u >: w' /\ v ==> w' /\ u -*> v)
-    ) -> w <:= w'
+    fun x => (x ==> w /\ x <: w) \/
+      (
+        exists u v : nat, is_sdom_of x u /\ node_in_fg u /\
+          node_in_fg v /\ u >: w /\ v ==> w /\ u -*> v
+      )
   )
 .
 Proof. Admitted.
